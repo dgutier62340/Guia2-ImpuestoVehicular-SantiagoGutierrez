35de576bf562ce2dc542654059a6073afadf55f2
Santiago Gutierrez <d.santiago34@hotmail.com>
2025-02-27 12:20:17 -0500

    Estructura java agregada

---
+++ src/main/MainImpuestoVehicular.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.impuestovehicular;
+
+/**
+ *
+ * @author Mystic
+ */
+public class ImpuestoVehicular {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
